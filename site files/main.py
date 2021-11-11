ee491e0fced38d4ebfa2694d21ec7076afdb595c
agoveahe <89888621+agoveahe@users.noreply.github.com>
2021-11-11 11:17:57 -0500

    Database

--- site files/main.py
+++ site files/main.py
@@ -10,7 +10,7 @@ from database import db
 from models import Post as Post
 from models import User as User
 app = Flask(__name__)     # create an app
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projectDB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 #  Bind SQLAlchemy db object to this Flask app
 db.init_app(app)
@@ -28,17 +28,17 @@ with app.app_context():
 @app.route('/')
 def index():
     a_user =  db.session.query(User).filter_by(email='placeholder@uncc.edu').one()
-    return render_template('index.html')
+    return render_template('index.html', user=a_user)
 @app.route('/posts')
 def get_posts():
     a_user = db.session.query(User).filter_by(email='placeholder@uncc.edu').one()
     my_posts = db.session.query(Post).all()
-    return render_template('posts.html', posts=my_posts)
+    return render_template('posts.html', posts=my_posts, user=a_user)
 @app.route('/posts/<post_id>')
 def get_post(post_id):
     a_user = db.session.query(User).filter_by(email='placeholder@uncc.edu').one()
     my_post = db.session.query(Post).filter_by(id=post_id).one()
-    return render_template('post.html', post=my_post)
+    return render_template('post.html', post=my_post, user=a_user)
 @app.route('/posts/new', methods=['GET', 'POST'])
 def new_post():
 
